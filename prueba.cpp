b981a885b41bbcd33dabc4e16c1a777677ecea1d
Paulo Monterroso <84726204+GiyuDev@users.noreply.github.com>
2022-11-24 03:41:34 -0600

    Initial commit

---
+++ prueba.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int suma(int num_1, int num_2) {
+
+	return (num_1 + num_2);
+
+}
+
+int main() {	
+	
+	int resultado = suma(2, 2);
+
+	cout<<"Hello"<<endl;
+	cout<<"A simple addition operation: "<<resultado<<endl;
+
+	return 0;
+
+}
